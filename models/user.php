a45897e65ec0d8ac704e135e62552113cc7aac3e
ei17ringo <ei17ringo@gmail.com>
2017-01-31 16:42:54 +0800

    init commit

---
+++ models/user.php
@@ -0,0 +1,17 @@
+<?php
+	class User{
+
+		//プロパティ（db接続オブジェクト)
+		private $dbconnect = '';
+
+		//コンストラクタ
+		// function __construct(){
+		// 	// DB接続ファイルを読み込む
+		// 	require('dbconnect.php');
+
+		// 	// DB接続設定の値をプロパティに代入
+		// 	$this->dbconnect = $db;
+		// }
+
+	}
+?>
