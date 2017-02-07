f2e7ba2c8ea35f7efed9c373d49db1d4234f58cf
MAI ICHINOKI <maimode080653@gmail.com>
2017-02-07 14:34:20 +0800

    Choose.php途中です

--- views/posts/choose.php
+++ views/posts/choose.php
@@ -28,17 +28,33 @@
                         <?php endif ?>
                           <div class="row-fluid">
                       <?php endif ?>
-                        <div class="span3"><a href="#x" class="thumbnail"><img src="../webroot/pictures/<?php echo $key['mainPictureAddress']; ?>" alt="Image" style="max-width:100%;" /></a></div>
+                        <div class="span3"><a href="#x" class="thumbnail"><img src="../webroot/pictures/<?php echo $key['mainPictureAddress']; ?>
                       <?php $i++; ?>
                     <?php endforeach ?>
                     </div><!--/row-fluid-->
                   </div><!--/item-->
                   <div class="item">
                     <div class="row-fluid">
-                      <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;" /></a></div>
-                      <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;" /></a></div>
-                      <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;" /></a></div>
-                      <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;" /></a></div>
+                       <?php
+                    $i = 0;
+                    foreach ($viewOptions as $key): ?>
+                      <?php if ($i != 0 && $i % 4 == 0): ?>
+                          </div><!-- row-fluid -->
+                        </div><!-- item -->
+                      <?php endif ?>
+                      <?PHP if ($i % 4 == 0): ?>
+                        <?PHP if ($i == 0): ?>
+                          <div class="item active">
+                        <?PHP else: ?>
+                            <div class="item">
+                        <?php endif ?>
+                          <div class="row-fluid">
+                      <?php endif ?>
+                        <div class="span3"><a href="#x" class="thumbnail"><img src="../webroot/pictures/<?php echo $key['mainPictureAddress']; ?>" alt="Image" style="max-width:100%;" /></a></div>
+                      <?php $i++; ?>
+                    <?php endforeach ?>
+                          </div><!-- item -->
+                        </div><!-- row-fluid -->
                     </div><!--/row-fluid-->
                   </div><!--/item-->
                   <div class="item">
